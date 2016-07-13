341e29f76e151cf819205f75951ddb07613d32e7
andy <andy2472625@gmail.com>
2016-07-13 23:34:11 +0800

    init

---
+++ js/common.js
@@ -0,0 +1,113 @@
+﻿$(document).ready(function(){
+	//Init
+	var o ={
+		wrp: $('.wrapper'),
+		ballrotateY: 0
+	};
+	// render();
+	function render(){
+		o.ballrotateY+=1;
+		if(o.ballrotateY >360) o.ballrotateY = 1;
+		$('.ball .bg').css('transform','rotateY('+o.ballrotateY+'deg)');
+		setTimeout(function(){
+			render();
+		},33);
+	}
+	
+})//ready end  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
